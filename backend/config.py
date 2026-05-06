b9d6873463cb60ca818138be5dee0a0a36774610
Gabriel <gabriel.carvalhovieira123@gmail.com>
2026-05-06 20:50:23 -0300

    modificando a senha do db

--- backend/config.py
+++ backend/config.py
@@ -7,6 +7,6 @@ class Config:
     HOST = '0.0.0.0'
     PORT = 3006
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:27052005@localhost:3306/pets"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Bancos*1999@localhost:3306/pets"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     #SECRET_KEY = 'chave_secreta'
