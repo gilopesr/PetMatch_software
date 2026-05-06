94af9530f86a4d7c5b7a400ef0b70e12663688db
Gabriel <gabriel.carvalhovieira123@gmail.com>
2026-05-06 20:54:06 -0300

    add img animais_model

--- backend/animais/animais_model.py
+++ backend/animais/animais_model.py
@@ -10,6 +10,7 @@ class Animais(db.Model):
     raca = db.Column(db.String(20), nullable=False)
     idade = db.Column(db.Integer, nullable=False)
     status = db.Column(db.String(20), nullable=False, default="disponivel")
+    img = db.Column(db.String(200),nullable=False)
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     responsavel = db.relationship('User', back_populates='pets')
