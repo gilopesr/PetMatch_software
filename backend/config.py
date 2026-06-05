d37a2ff39894cf20098d2492a3e79026077cb5c7
Gabriel <gabriel.carvalhovieira123@gmail.com>
2026-06-05 17:25:35 -0300

    localhost para db, e colocando db = SQLAlchemy no fim do arquivo

--- backend/config.py
+++ backend/config.py
@@ -1,13 +1,13 @@
 import os
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
-
 class Config:
     HOST = '0.0.0.0'
     PORT = 8000
     DEBUG = True
     #SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Bancos*1999@localhost:3306/pets"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:27052005@localhost:3306/pets"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Bancos*1999@db:3306/data_pets"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     #SECRET_KEY = 'chave_secreta'
+
+db = SQLAlchemy()
